af695b46101b642fd79d7b991177cd23dccdd2a5
trannpbtb00387-bit <trannpbtb00387@fpt.edu.vn>
2025-09-30 08:37:30 +0700

    Lab3_bai2

--- Lab3/src/main/java/com/mycompany/lab3/bai2.java
+++ Lab3/src/main/java/com/mycompany/lab3/bai2.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.lab3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ICT
@@ -11,6 +13,47 @@ package com.mycompany.lab3;
 public class bai2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("===== MENU =====");
+            System.out.println("1. In 1 bang nhan");
+            System.out.println("2. In toan bo 9 bang nhan");
+            System.out.println("0. Thoat");
+            System.out.print("Moi ban chon: ");
+            int choice = sc.nextInt();
+
+            if (choice == 0) {
+                System.out.println("Chuong trinh ket thuc!");
+                break;
+            }
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Nhap so muon in bang nhan: ");
+                    int x = sc.nextInt();
+                    System.out.println("Bang nhan " + x);
+                    for (int i = 1; i <= 10; i++) {
+                        System.out.printf("%d x %d = %d\n", x, i, x * i);
+                    }
+                    System.out.println();
+                    break;
+
+                case 2:
+                    for (int i = 1; i <= 9; i++) {
+                        System.out.println("Bang nhan " + i);
+                        for (int j = 1; j <= 10; j++) {
+                            System.out.printf("%d x %d = %d\n", i, j, i * j);
+                        }
+                        System.out.println();
+                    }
+                    break;
+
+                default:
+                    System.out.println("Lua chon khong hop le, vui long thu lai!");
+            }
+        }
+
+        sc.close();
     }
 }
